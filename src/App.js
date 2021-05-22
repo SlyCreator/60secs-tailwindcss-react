1c33fc266ae54cd4bf700d36a5704b705505df99
slycreator <chinonso1amaechi@gmial.com>
2021-05-22 06:28:02 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div className="max-w-sm rounded overflow-hidden shadow-lg mx-auto my-8">
+          <img className="w-full" src="https://tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains" />
+              <div className="px-6 py-4">
+                  <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+                  <p className="text-gray-600 text-base">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et
+                      perferendis eaque, exercitationem praesentium nihil.
+                  </p>
+              </div>
+              <div className="px-6 py-4">
+                  <span
+                      className="inline-block bg-gray-100 rounded-full px-3 py-1 text-sm font-semibold text-gray-600 mr-2">#photography</span>
+                  <span
+                      className="inline-block bg-gray-100 rounded-full px-3 py-1 text-sm font-semibold text-gray-600 mr-2">#travel</span>
+                  <span
+                      className="inline-block bg-gray-100 rounded-full px-3 py-1 text-sm font-semibold text-gray-600">#winter</span>
+              </div>
+      </div>
 
+)}
 export default App;
